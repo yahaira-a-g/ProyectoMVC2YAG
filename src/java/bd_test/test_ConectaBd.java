3f43ea8ad753f3f40052109b17a89f340171c229
Yahaira Adama <yahairaadamagalarza@gmail.com>
2020-08-03 09:11:49 -0500

    Inicalizando MVC

---
+++ src/java/bd_test/test_ConectaBd.java
@@ -0,0 +1,18 @@
+package bd_test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import bd.ConectaBd;
+
+public class test_ConectaBd {
+
+    public static void main(String[] args) {
+        ConectaBd bd = new ConectaBd();
+        try (Connection cn = bd.getConnection()) {
+            System.out.println("Conexion OK..)");
+        } catch (SQLException e) {
+            System.out.println("Error; ");
+            System.out.println(e.getMessage());
+        }
+    }
+}
